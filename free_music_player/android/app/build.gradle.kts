bac97c4b077f73fcadc0fa664a91e0c0871e0eca
Daniel Serrano Cañas <51306347+dansecan25@users.noreply.github.com>
2025-10-21 19:18:25 -0600

    [UPDATE] flutter version

--- free_music_player/android/app/build.gradle.kts
+++ free_music_player/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.free_music_player"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
